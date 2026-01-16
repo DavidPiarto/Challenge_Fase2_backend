f142851bfd587c58d03090e31add490a1f983417
David <david.custodio2009@gmail.com>
2026-01-16 14:37:05 -0300

    Teste CICD

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const PORT = 3000;
 
 // MIDDLEWARES
 app.use((req, res, next) => {
-    console.log(`Método: ${req.method} - URL: ${req.url}`);
+    console.log(`Método: ${req.method} - URL: ${req.url} !`);
     next();
 });
 
